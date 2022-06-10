5e2caa0c74fe0022bfd200e42802c26165045a75
Lelouch <animafoxy05@gmail.com>
2022-06-10 12:03:39 -0500

    Frontend

--- hermes.of.cheese.backend/src/main/java/com/sena/proyect/hermes/of/cheese/presentation/controller/DeliveryController.java
+++ hermes.of.cheese.backend/src/main/java/com/sena/proyect/hermes/of/cheese/presentation/controller/DeliveryController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/delivery")
 @SecurityRequirement(name = "bearerAuth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class DeliveryController {
 
     @Autowired
--- hermes.of.cheese.backend/src/main/java/com/sena/proyect/hermes/of/cheese/presentation/controller/ProductController.java
+++ hermes.of.cheese.backend/src/main/java/com/sena/proyect/hermes/of/cheese/presentation/controller/ProductController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/product")
 @SecurityRequirement(name = "bearerAuth")
+@CrossOrigin(origins = "*")
 public class ProductController {
     @Autowired
     private ProductCrudService productService;
