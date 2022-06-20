1b269e131832bcfc4c5a531d56b1b278e95c7625
Lelouch <animafoxy05@gmail.com>
2022-06-20 17:38:06 -0500

    Final update

--- hermes-frontend/src/app/model/jwt-auth-response.ts
+++
@@ -1,9 +0,0 @@
-export class JwtAuthResponse {
-    type: string;
-    token: string;
-
-    constructor(type: string, token: string) {
-        this.type = type;
-        this.token = token;
-    }
-}
